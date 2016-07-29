23ba0ccc483fafe1e194bffc8d48309b9ec9ac87
rio45ka <rio.aska35@gmail.com>
2016-07-29 23:17:23 +0700

    Change README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CobaFirebase
+
+Ini source code dari tutorial di https://kelaskita.com/rioaskapermana/kelas/belajar-android-mencoba-membuat-notification-menggunakan-firebase-cloud-messaging/
--- app/src/main/java/com/aska/rio/cobafirebase/MainActivity.java
+++ app/src/main/java/com/aska/rio/cobafirebase/MainActivity.java
@@ -5,9 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Log.d("token firebase", FirebaseInstanceId.getInstance().getToken());
 
     }
 
